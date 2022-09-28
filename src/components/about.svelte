3806a4bad5b157ceda36337f8ea295912bbad59b
DaviDemarqui <davidemarqui@genesyssoft.com.br>
2022-09-28 11:55:53 -0300

    first prototype

---
+++ src/components/about.svelte
@@ -0,0 +1,22 @@
+<section class="bg-white">
+    <div class="gap-16 items-center py-8 px-4 mx-auto max-w-screen-xl lg:grid lg:grid-cols-2 lg:py-16 lg:px-6">
+        <div class="font-light text-gray-500 sm:text-lg">
+            <h2 class="mb-4 text-4xl tracking-tight font-extrabold text-gray-900">We didn't reinvent the wheel</h2>
+            <p class="mb-4">We are strategists, designers and developers. Innovators and problem solvers. Small enough to be simple and quick, but big enough to deliver the scope you want at the pace you need. Small enough to be simple and quick, but big enough to deliver the scope you want at the pace you need.</p>
+            <p>We are strategists, designers and developers. Innovators and problem solvers. Small enough to be simple and quick.</p>
+        </div>
+        <div class="grid grid-cols-2 gap-4 mt-8">
+            <img class="w-full rounded-lg" src="https://flowbite.s3.amazonaws.com/blocks/marketing-ui/content/office-long-2.png" alt="office content 1">
+            <img class="mt-4 w-full lg:mt-10 rounded-lg" src="https://flowbite.s3.amazonaws.com/blocks/marketing-ui/content/office-long-1.png" alt="office content 2">
+        </div>
+    </div>
+</section>
+<section class="bg-white">
+    <div class="py-8 px-4 mx-auto max-w-screen-xl lg:py-16 lg:px-6">
+        <div class="max-w-screen-lg text-gray-500 sm:text-lg">
+            <h2 class="mb-4 text-4xl tracking-tight font-bold text-gray-900">Powering innovation at <span class="font-extrabold">200,000+</span> companies worldwide</h2>
+            <p class="mb-4 font-light">Track work across the enterprise through an open, collaborative platform. Link issues across Jira and ingest data from other software development tools, so your IT support and operations teams have richer contextual information to rapidly respond to requests, incidents, and changes.</p>
+            <p class="mb-4 font-medium">Deliver great service experiences fast - without the complexity of traditional ITSM solutions.Accelerate critical development work, eliminate toil, and deploy changes with ease.</p>
+        </div>
+    </div>
+  </section>
---
+++ src/routes/index.svelte
@@ -0,0 +1,15 @@
+<script>
+    import Header from "../components/header.svelte";
+    import Hero from "../components/hero.svelte";
+    import Testimonial from "../components/testimonial.svelte";
+    import About from "../components/about.svelte";
+    import Pricing from "../components/pricing.svelte";
+    import Footer from "../components/footer.svelte";
+</script>
+
+<Header/>
+<Hero/>
+<Testimonial/>
+<About />
+<Pricing />
+<Footer/>
